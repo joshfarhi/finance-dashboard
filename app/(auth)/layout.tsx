79e87fbda92efabaf911ebcfd084cc40b13db800
joshfarhi <joshuafarhi@protonmail.com>
2024-10-11 16:00:13 -0400

    31:18 stopping point

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="flex min-h-screen w-full justify-between font-inter">
+      {children}
+      <div className="auth-asset">
+        <div>
+        </div>
+      </div>
+    </main>
+  );
+}
